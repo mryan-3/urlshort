70f8837ec13430071d391c73de2c825dd4354e1e
mryan-3 <malawaryan@gmail.com>
2024-01-23 20:33:22 +0300

    hero section

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 const Home = () => {
     return (
-        <div className="w-full h-screen text-[#e3f6f5]">
-            <h1 className="text-lg font-bold flex justify-center">Hello from Suplex City</h1>
+        <div className="w-full h-[600px] text-[#e3f6f5]">
+            <h1 className="text-5xl font-mono font-extrabold text-center py-10">GoTiny</h1>
+            <div className="text-xl font-serif font-thin text-center leading-relaxed flex justify-center">
+                <p>Go Tiny is a tool that empowers you to create short, consise and shareable links. Say goodbye to long links and hello to streamlined and shareable URLs.
+            </p>
+            </div>
         </div>
     )
 }
--- frontend/src/components/Offers.jsx
+++ frontend/src/components/Offers.jsx
@@ -1,37 +1,38 @@
 import React from "react";
-import {FaThumbsUp} from 'react-icons/fa6'
+import {FaThumbsUp, FaHand} from 'react-icons/fa6'
+import {AiOutlineSafety} from 'react-icons/ai'
 
 const Offers = () => {
     return(
     <div className="w-full items-center text-[#e3f6f5] ">
-            <h3 className="text-center text-[#e3f6f5]">What we offer</h3>
+            <h3 className="text-center font-bold font-mono text-4xl py-10  text-[#e3f6f5]">What we offer</h3>
         <div className="flex flex-col md:grid grid-rows-2 grid-cols-3 ">
-        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#272643]">
+        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#bae8e8] text-[#272643]">
             <FaThumbsUp size={50}/>
             <h3>Easy to use</h3>
             <p>We offer an easy to use and navigate service</p>
         </div>
-        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#272643]">
+        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl  bg-[#bae8e8] text-[#272643]">
+            <AiOutlineSafety size={50}/>
+            <h3>Easy to use</h3>
+            <p>We offer an easy to use and navigate service</p>
+        </div>
+        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#bae8e8] text-[#272643]">
+            <FaHand size={50}/>
+            <h3>Easy to use</h3>
+            <p>We offer an easy to use and navigate service</p>
+        </div>
+        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#bae8e8] text-[#272643]">
             <FaThumbsUp size={50}/>
             <h3>Easy to use</h3>
             <p>We offer an easy to use and navigate service</p>
         </div>
-        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#272643]">
+        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#bae8e8] text-[#272643]">
             <FaThumbsUp size={50}/>
             <h3>Easy to use</h3>
             <p>We offer an easy to use and navigate service</p>
         </div>
-        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#272643]">
-            <FaThumbsUp size={50}/>
-            <h3>Easy to use</h3>
-            <p>We offer an easy to use and navigate service</p>
-        </div>
-        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#272643]">
-            <FaThumbsUp size={50}/>
-            <h3>Easy to use</h3>
-            <p>We offer an easy to use and navigate service</p>
-        </div>
-        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#272643]">
+        <div className="flex flex-col items-center border-2 mx-10 md:mx-8 my-3 p-4 rounded-xl bg-[#bae8e8] text-[#272643]">
             <FaThumbsUp size={50}/>
             <h3>Easy to use</h3>
             <p>We offer an easy to use and navigate service</p>
