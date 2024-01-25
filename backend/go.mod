bd2c9460d21cc09088a3826ffee3b8b03d865a54
mryan-3 <malawaryan@gmail.com>
2024-01-25 10:47:27 +0300

    Added a hash function to convert the url given to a short url

--- backend/go.mod
+++ backend/go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
+	github.com/webermarci/base62 v0.17.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
--- backend/main.go
+++ backend/main.go
@@ -2,21 +2,72 @@ package main
 
 import (
 	"context"
+    "strings"
+	"crypto/sha256"
 	"fmt"
 	"log"
-
+    "encoding/binary"
+    "github.com/webermarci/base62"
+	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 
-type Book struct{
-    Title string
-    Author string
+type ReturnedUrl struct{
+    Key string `json:"key"`
+    Short_Url string `json:"short_url"`
+    Full_Url string `json:"full_url"`
 }
 
-func main() {
-    var collection *mongo.Collection
+type Url struct{
+    Url string `json:"url"`
+}
+func main(){
+    hashUrl("https://www.youtube.com/watch=?jhhfsaf")
+}
+
+func initRouter(){
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello World Jones")
+    } )
+
+
+    app.Post("/", func(c *fiber.Ctx) error {
+        body := new(Url)
+        if err := c.BodyParser(body); err != nil {
+            c.Status(fiber.StatusBadRequest).SendString(err.Error())
+            return err
+        }
+       return nil
+    })
+
+    app.Listen("localhost:3001")
+}
+
+func hashUrl(url string){
+    h := sha256.New()
+    h.Write([]byte(url))
+    bs := h.Sum(nil)
+    keyBytes := bs[:8]
+    fmt.Println(keyBytes)
+
+    uintBytes := binary.LittleEndian.Uint64(keyBytes)
+
+    encoded := base62.Encode(uintBytes)
+    encoded = encoded[:6]
+    key := strings.ToLower(encoded)
+
+    short_url := "https://ryan/" + key
+    fmt.Println(short_url)
+}
+
+
+
+func initDB() {
+ //   var collection *mongo.Collection
     ctx := context.TODO()
     clientOptions := options.Client().ApplyURI("mongodb://localhost:27017/")
     client, err := mongo.Connect(ctx, clientOptions)
@@ -33,12 +84,8 @@ func main() {
         log.Fatal(err)
     }
 
-    collection = client.Database("NotesPool").Collection("Notes")
-    doc := Book{
-        Title: "Harry Potter",
-        Author: "JK Rowling",
-    }
+//    collection = client.Database("NotesPool").Collection("Notes")
 
-    result, err := collection.InsertOne(ctx, doc)
-    fmt.Printf("Inserted document with _id: %v \n", result.InsertedID)
+   // result, err := collection.InsertMany(ctx, docs)
+   // fmt.Printf("Inserted document with _id: %v \n", result.InsertedIDs...)
 }
