d5e3e6875d5646e28984f0fcee98645cf679257d
mryan-3 <malawaryan@gmail.com>
2024-02-03 19:13:22 +0300

    Refactored the code for the routes to their own directory

---
+++ backend/routes/routes.go
@@ -0,0 +1,113 @@
+package routes
+
+import (
+	"context"
+	"crypto/sha256"
+
+	//	"fmt"
+	"strings"
+
+	//	"fmt"
+	"encoding/binary"
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/urlshort/database"
+	"github.com/webermarci/base62"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"log"
+)
+
+type PostUrl struct {
+	Url string `json:"url"`
+}
+
+type StoredUrl struct {
+	Key      string `json:"key"`
+	ShortUrl string `json:"short_url"`
+	LongUrl  string `json:"long_url"`
+}
+
+func InitRouter() {
+	app := fiber.New()
+	app.Use(logger.New())
+
+	app.Get("/:id?", func(c *fiber.Ctx) error {
+		collection, err := database.GetMongoDbCollection("JsonParser", "Go")
+		if err != nil {
+			log.Println(err)
+		}
+
+		var filter bson.M = bson.M{}
+
+		if c.Params("id") != "" {
+			id := c.Params("id")
+
+			objID, _ := primitive.ObjectIDFromHex(id)
+			filter = bson.M{"_id": objID}
+		}
+
+		var results []bson.M
+		cur, err := collection.Find(context.Background(), filter)
+		defer cur.Close(context.Background())
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		cur.All(context.Background(), &results)
+
+		if results == nil {
+			c.SendStatus(404)
+		}
+
+		json, _ := json.Marshal(results)
+		return c.Send(json)
+
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		var url PostUrl
+		json.Unmarshal([]byte(c.Body()), &url)
+
+		key, short_url, long_url := HashUrl(url.Url)
+
+		storedUrl := StoredUrl{
+			Key:      key,
+			ShortUrl: short_url,
+			LongUrl:  long_url,
+		}
+
+		collection, err := database.GetMongoDbCollection("JsonParser", "Go")
+		if err != nil {
+			log.Println(err)
+		}
+		res, err := collection.InsertOne(context.TODO(), storedUrl)
+		log.Println(res)
+		if err != nil {
+			log.Println(err)
+		}
+		response, _ := json.Marshal(res)
+		return c.Send(response)
+	})
+
+	app.Listen("localhost:3001")
+}
+
+func HashUrl(url string) (key string, short string, original string) {
+	h := sha256.New()
+	h.Write([]byte(url))
+	bs := h.Sum(nil)
+	keyBytes := bs[:8]
+	//    fmt.Println( keyBytes)
+	uintBytes := binary.LittleEndian.Uint64(keyBytes)
+
+	encoded := base62.Encode(uintBytes)
+	encodedShort := encoded[:6]
+	keyLower := strings.ToLower(encodedShort)
+
+	shortened := "https://ryan/" + keyLower
+
+	return keyLower, shortened, url
+}
